aa11597fd3a666b8ce0683991e5d2ee778136bf0
anshif-8590 <anshifp949@gmail.com>
2026-04-28 17:29:08 +0530

    made some changes i the import of images from assets file

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('db_penggajian3', 'root', '', {
+const db = new Sequelize('employee_salary_db', 'root', '1234', {
     host: "localhost",
     dialect: "mysql"
 });
